3e8b349d213d732275e984dfee743b83a604ac14
Benia Morgan <bmorganware@gmail.com>
2024-08-03 11:37:04 -0400

    First commit

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Store
+{
+    private static void Main(string[] args)
+    {
+        int sodaCurrentStockValue = 100;
+        int chipsCurrentStockValue = 40;
+        int candyCurrentStockValue = 60;
+
+        int sodaRestockMinimum = 40;
+        int chipsRestockMinimum = 20;
+        int candyRestockMinimum = 40;
+
+        Console.WriteLine("Welcome to the Restocking tool");
+        Console.WriteLine($"How many sodas have been sold today? {sodaCurrentStockValue} are currently in stock");
+
+        int userInput = int.Parse(Console.ReadLine());
+
+        var sodaRemainingStock = sodaCurrentStockValue - userInput;
+
+        Console.WriteLine($"How many chips have been sold today? {chipsCurrentStockValue} are currently in stock");
+
+        userInput = int.Parse(Console.ReadLine());
+    }
+}
